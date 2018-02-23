63768b8794c7d2260ec38681d605bd5c2f7d3f64
Mike <mike@sagefoc.us>
2018-02-23 17:26:29 -0600

    gofmt

--- env.go
+++ env.go
@@ -1,28 +1,28 @@
 package main
 
 type Environment struct {
-    emuCtrlr *Controller
+	emuCtrlr *Controller
 }
 
 func NewEnvironment(cmdArr []string, mapPath string) *Environment {
-    env := &Environment{
-        emuCtrlr: NewController(cmdArr, mapPath),
-    }
-    return env
+	env := &Environment{
+		emuCtrlr: NewController(cmdArr, mapPath),
+	}
+	return env
 }
 
 func (env *Environment) Init() {
-    env.emuCtrlr.Init()
+	env.emuCtrlr.Init()
 }
 
 func (env *Environment) GetState(state []float64) {
-    env.emuCtrlr.GetState(state)
+	env.emuCtrlr.GetState(state)
 }
 
 func (env *Environment) GameStep(action uint64) {
-    env.emuCtrlr.GameStep(action)
+	env.emuCtrlr.GameStep(action)
 }
 
 func (env *Environment) LoadGame() {
-    env.emuCtrlr.LoadGame()
+	env.emuCtrlr.LoadGame()
 }
--- main.go
+++ main.go
@@ -1,126 +1,126 @@
 package main
 
 import (
-    "os"
-    "fmt"
+	"fmt"
+	"os"
 
-    "gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/mat"
 
-    "github.com/SageFocusLLC/gophernet"
+	"github.com/SageFocusLLC/gophernet"
 )
 
 //has to be mupen64plus 64 bit linux, with default input plugin
-func main(){
-    args := os.Args[1:]
-    mapPath := "./statemap.json"
-    env := NewEnvironment(args, mapPath)
+func main() {
+	args := os.Args[1:]
+	mapPath := "./statemap.json"
+	env := NewEnvironment(args, mapPath)
 
-    env.Init()
+	env.Init()
 
-    //create neural net
-    nnConfig := gonet.NeuralNetConfig{
-        InputNeurons: 4,
-        OutputNeurons: 8,
-        HiddenNeurons: 10,
-        NumEpochs: 1,
-        LearningRate: 0.1,
-    }
+	//create neural net
+	nnConfig := gonet.NeuralNetConfig{
+		InputNeurons:  4,
+		OutputNeurons: 8,
+		HiddenNeurons: 10,
+		NumEpochs:     1,
+		LearningRate:  0.08,
+	}
 
-    agent := NewAgent(nnConfig)
+	agent := NewAgent(nnConfig)
 
-    agent.LoadNN()
+	agent.LoadNN()
 
-    stateArr := []float64{0.01, 0.01, 0.01, 0.01}
-    env.GetState(stateArr)
+	stateArr := []float64{0.01, 0.01, 0.01, 0.01}
+	env.GetState(stateArr)
 
-    reward := 0.0
-    epoch := 0
-    action := uint64(0x00)
-    mapPositionVec(stateArr[0:3])
+	reward := 0.0
+	epoch := 0
+	action := uint64(0x00)
+	mapPositionVec(stateArr[0:3])
 
-    stateP := mat.NewDense(1, len(stateArr), nil)
-    state  := mat.NewDense(1, len(stateArr), nil)
+	stateP := mat.NewDense(1, len(stateArr), nil)
+	state := mat.NewDense(1, len(stateArr), nil)
 
-    for epoch < 100{
-        fmt.Println("Epoch ", epoch)
+	for epoch < 100 {
+		fmt.Println("Epoch ", epoch)
 
-        env.LoadGame()
+		env.LoadGame()
 
-        endstate := false
-        step := 0
+		endstate := false
+		step := 0
 
-        for step < 2000 && endstate != true{
-            //action
-            state.SetRow(0, stateArr)
+		for step < 2000 && endstate != true {
+			//action
+			state.SetRow(0, stateArr)
 
-            action = agent.GetAction(state)
+			action = agent.GetAction(state)
 
-            env.GameStep(action)
+			env.GameStep(action)
 
-            //observation
-            env.GetState(stateArr)
+			//observation
+			env.GetState(stateArr)
 
-            mapPositionVec(stateArr[0:3])
+			mapPositionVec(stateArr[0:3])
 
-            stateArr[3] = stateArr[3] * 0.01
+			stateArr[3] = stateArr[3] * 0.01
 
-            //reward
-            reward, endstate = getReward(stateArr, epoch, step)
+			//reward
+			reward, endstate = getReward(stateArr, epoch, step)
 
-            stateP.SetRow(0, stateArr)
+			stateP.SetRow(0, stateArr)
 
-            agent.GiveReward(state,stateP,reward)
+			agent.GiveReward(state, stateP, reward)
 
-            step += 1
-        }
-        agent.SaveNN()
+			step += 1
+		}
+		fmt.Println(agent.Q)
+		agent.SaveNN()
 
-        epoch += 1
-    }
-    fmt.Println("done :D")
+		epoch += 1
+	}
+	fmt.Println("done :D")
 }
 
 func getReward(stateArr []float64, epoch int, step int) (float64, bool) {
-    reward := 0.0
-    endstate := false
+	reward := 0.0
+	endstate := false
 
-    if stateArr[1] > 0.27055 && stateArr[1] < 0.28270 {
-        if stateArr[0] > 0.12314 && stateArr[0] < 0.24100 {
-            if stateArr[2] < 0.82216 && stateArr[2] > 0.70840 {
-                reward = 5.0
-                fmt.Println("WIN")
-                fmt.Println(stateArr)
-                endstate = true
-            }
-        }
-    }
+	if stateArr[1] > 0.27055 && stateArr[1] < 0.28270 {
+		if stateArr[0] > 0.12314 && stateArr[0] < 0.24100 {
+			if stateArr[2] < 0.82216 && stateArr[2] > 0.70840 {
+				reward = 5.0
+				fmt.Println("WIN")
+				fmt.Println(stateArr)
+				endstate = true
+			}
+		}
+	}
 
-    //fell off the level
-    if stateArr[1] < 0.27 {
-        reward = -10.0
-        fmt.Println("Fell")
-        endstate = true
-    }
+	//fell off the level
+	if stateArr[1] < 0.27 {
+		reward = -1.0
+		fmt.Println("Fell")
+		endstate = true
+	}
 
-    //avoid start area
-    if stateArr[1] > 0.791 {
-        reward = -1.0
-    } else {
-        reward = -0.5
-    }
-
-    reward += stateArr[2] * 0.5
-
-    return reward, endstate
+	reward = stateArr[1]*-0.8 + stateArr[3]*0.2
+	//avoid start area
+	if stateArr[0] > 0.64 {
+		reward += -0.5
+	} else {
+		reward += -0.05
+	}
+	reward = reward * 0.3
+	return reward, endstate
 
 }
 
-func mapPositionVec(fArr []float64){
-    for i, v := range fArr{
-        fArr[i] = mapPositionVal(v)
-    }
+func mapPositionVec(fArr []float64) {
+	for i, v := range fArr {
+		fArr[i] = mapPositionVal(v)
+	}
 }
 
-func mapPositionVal(x float64) float64{
-    return (x / 20000.0) + 0.5
+func mapPositionVal(x float64) float64 {
+	return (x / 20000.0) + 0.5
 }
